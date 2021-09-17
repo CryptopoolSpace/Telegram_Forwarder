0e1e3bf6ea33b375eab0615bd9ce7b37e8229589
fariz tiger <tigerhyip@gmail.com>
2021-09-17 18:04:20 +0800

    init

--- forwarder/sample_config.py
+++ forwarder/sample_config.py
@@ -15,12 +15,12 @@ class Config(object):
     REMOVE_TAG = False
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "1992440646:AAFZa6W6xS7xedq2ytoCARtPFXF6k10A970"  # API key obtained from BotFather
+    OWNER_ID = "686960420"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
+    FROM_CHATS = [-212686814]  # List of chat id's to forward messages from
+    TO_CHATS = [-1001460142796]  # List of chat id's to forward messages to
 
     # FOR WEBHOOKS
     WEBHOOK = False
